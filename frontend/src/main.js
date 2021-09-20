fd12fec8636d5f732d8dbe6cb62dc38a27661172
Kristian Freeman <kristian@kristianfreeman.com>
2021-09-20 09:46:30 -0500

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,5 @@
 import App from './App.svelte';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+const app = new App({ target: document.body });
 
 export default app;
